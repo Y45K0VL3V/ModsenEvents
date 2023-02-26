ed3a912ca0ddcd94f5d3216c3c05465242458afa
Y45K0VL3V <yakovlevvadim666@gmail.com>
2023-02-26 16:43:57 +0300

    Add shared result models

---
+++ Domain/Shared/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Shared
+{
+    public class Result
+    {
+        public Result(bool isSuccess, Error error) 
+        {
+            if (!(isSuccess ^ error == Error.None))
+                throw new InvalidOperationException();
+
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public bool IsSuccess { get; private set; } 
+        public Error Error { get; private set; }
+
+        public static Result Success() => new(true, Error.None);
+    }
+}
---
+++ Domain/Shared/ResultGeneric.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Shared
+{
+    public class Result<T> : Result
+    {
+        public Result(T? value, bool isSuccess, Error error)
+            : base(isSuccess, error)
+        {
+            _value = value;
+        }
+
+        private readonly T? _value;
+        public T Value => IsSuccess 
+            ? _value 
+            : throw new InvalidOperationException("Value of failure can't be accepted."); 
+    }
+}
